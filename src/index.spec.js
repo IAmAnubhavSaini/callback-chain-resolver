0eadd8d585804c5f1f42a75670b916b782fa48c3
Anubhav <iamanubhavsaini+github@gmail.com>
2021-05-23 15:25:19 +0530

    Closes #5. Removed a bunch of dev deps

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,13 +1,12 @@
-const expect = require('chai').expect
-const resolver = require('./index')
+const resolver = require('./index');
 
 describe('callback-chain-resolver', () => {
 
     const incrementBy1 = (x) => x + 1;
     const decrementBy1 = (x) => x - 1;
-    const double = x => 2 * x
-    const square = x => x * x
-    const cube = x => x * square(x)
+    const double = x => 2 * x;
+    const square = x => x * x;
+    const cube = x => x * square(x);
 
     it('resolver() resolves successfully', () => {
         [{
@@ -15,22 +14,22 @@ describe('callback-chain-resolver', () => {
             initValue: 1,
             expected: 64
         },
-        {
-            callbackArray: [double, square, decrementBy1, decrementBy1, decrementBy1],
-            initValue: 1,
-            expected: 1
-        },
-        {
-            callbackArray: [incrementBy1, cube, decrementBy1],
-            initValue: 1,
-            expected: 7
-        },
-        {
-            callbackArray: [decrementBy1, incrementBy1],
-            initValue: 1,
-            expected: 1
-        }].forEach(testCase => {
-            expect(resolver(testCase.initValue, testCase.callbackArray)).to.equal(testCase.expected)
-        })
-    })
-})
+            {
+                callbackArray: [double, square, decrementBy1, decrementBy1, decrementBy1],
+                initValue: 1,
+                expected: 1
+            },
+            {
+                callbackArray: [incrementBy1, cube, decrementBy1],
+                initValue: 1,
+                expected: 7
+            },
+            {
+                callbackArray: [decrementBy1, incrementBy1],
+                initValue: 1,
+                expected: 1
+            }].forEach(testCase => {
+            expect(resolver(testCase.initValue, testCase.callbackArray)).toEqual(testCase.expected);
+        });
+    });
+});
