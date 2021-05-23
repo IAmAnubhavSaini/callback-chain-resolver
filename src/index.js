2441975938b654845e10858b128b8a30106d3690
Anubhav <iamanubhavsaini+github@gmail.com>
2021-05-23 15:33:33 +0530

    v13

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const resolver = (initValue, callbacks) => callbacks.reduce((accumulated, cb) => cb(accumulated), initValue);
+const resolver = (initValue, callbacks) => callbacks.reduce((value, cb) => cb(value), initValue);
 
 module.exports = resolver;
